5cc5e74a30fe103dcf1a660b8a49564473f31f7b
Matías Benjamín Moreno Juárez <matt@iMac-de-Matias.local>
2020-08-30 13:54:12 +0100

    getting recipe from second api call, installing Material-UI for modals

--- src/context/ModalContext.js
+++ src/context/ModalContext.js
@@ -1,4 +1,5 @@
 import React, { createContext, useEffect, useState } from 'react';
+import Axios from 'axios';
 
 export const ModalContext = createContext();
 
@@ -6,6 +7,19 @@ const ModalProvider = (props) => {
 
     //Provider State
     const [idRecipe, setIdRecipe] = useState(null);
+    const [recipe, setRecipe] = useState({}); //save the actual recipe
+
+    //Once recipe is selected, get it from API
+    useEffect(() => {
+        const getRecipe = async () => {
+            if (!idRecipe) return;
+            const recipeUrl = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idRecipe}`;
+            const response = await Axios(recipeUrl);
+            setRecipe(response.data.drinks[0])
+            // console.log(response.data.drinks)
+        }
+        getRecipe();
+    }, [idRecipe])
 
     return ( 
         <ModalContext.Provider
