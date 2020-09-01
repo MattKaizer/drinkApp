e48275406cb634cea567599fe02b3c403aa5ee7d
Matías Benjamín Moreno Juárez <matt@iMac-de-Matias.home>
2020-09-01 20:23:42 +0100

    modal fix

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -4,7 +4,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 import Backdrop from '@material-ui/core/Backdrop';
 import Fade from '@material-ui/core/Fade';
-import { TextareaAutosize } from '@material-ui/core';
 
 
 
